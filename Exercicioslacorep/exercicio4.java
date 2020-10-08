105128969b5f371c7f870fd85386f46d571dd734
lucas Trevizan <lucasevizan@hotmail.com>
2020-10-08 16:06:53 -0300

    versao3

--- Exercicioslacorep/exercicio4.java
+++ Exercicioslacorep/exercicio4.java
@@ -6,7 +6,7 @@ public class exercicio4 {
 		Scanner ler = new Scanner(System.in);
 	    int idade,sexo,ep,pc=0,oc=0,mc=0,mn=0,qn=0,ha=0,pessoas=1;
 	    
-	    while(pessoas<=3)
+	    while(pessoas<=6)
 	    {
 	    	System.out.println("Entre com sua idade: ");
 		    idade = ler.nextInt();
@@ -14,31 +14,32 @@ public class exercicio4 {
 		    sexo= ler.nextInt();
 		    System.out.println("Se  você for calmo digite 1, se for nervosa digite 2 e se for agressiva digite 3");
 		    ep = ler.nextInt();
-		    pessoas ++;
 	    	if(ep==1)
 	    	{
 	    		pc++;
-	    		if(ep==1 && sexo ==3)
-	    		{
-	    		  oc++;	
-	    		}
-	    		if(idade<18 && ep==1)
-	    		{
-	    			mc++;
-	    		}
-	    	}else if(sexo==1 && ep==2)
+	    	}
+	    	if(ep==1 && sexo ==3)
+	    	{
+	            oc++;	
+	    	}
+	    	if(idade<18 && ep==1)
+	    	{
+	    	    mc++;
+	    	}
+	    	if(sexo==1 && ep==2)
 	    	{
 	    		mn++;
-	    		if(idade>40 && ep==2)
-	    		{
-	    			qn++;
-	    		}
+	    	}
+	    	if(idade>40 && ep==2)
+	    	{
+	    	   qn++;
+	    	}
 	    			
-	    	}else if(sexo==2 && ep==3)
+	    	if(sexo==2 && ep==3)
 	    	{
 	    		ha++;
 	    	}
-	    	
+	    	pessoas ++;
 	    }
 	    System.out.printf("O numero de pessoas calmas  é de: %d, o número de pessos do sexo(outros) calmos é de: %d, o numero de pessoas com menos de 18 anos e calmas é de:%d",pc,oc,mc);
 	    System.out.printf("\n O número de mulheres nervosas é  de: %d, o número de pessoas com mais de 40 anos nervosas é de: %d",mn,qn);
